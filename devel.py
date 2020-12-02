782793c5653b1958445bfd075d7954a814eb6779
Ivin <rdrf.myfoot@gmail.com>
2020-12-02 16:39:05 +0000

    Removed files in wrong location

--- devel.py
+++ devel.py
@@ -6,7 +6,6 @@ import numpy as np
 import mujoco_py
 import xml.etree.ElementTree as ET
 
-from passive_hand_env.paramatrize import Paramatrizer
 
 kwargs = {
     'reward_type': 'sparse',
@@ -25,13 +24,3 @@ env.sim.model.body_mass[-4] = 100 # this changes the model in this session
 # ie. changes preserved even after env.reset(), but doesn't change contents in xml files
 env.reset()
 
-prm = Paramatrizer(env) 
-
-for i in range(3):
-    print(prm.change_object_pos())
-    env.reset()
-    for _ in range(100):
-        env.render()
-        o,r,d,i = env.step(env.action_space.sample()) # take a random action
-
-env.close()
--- simulation/dm_control/devel.py
+++ simulation/dm_control/devel.py
@@ -9,3 +9,5 @@ def random_policy(time_step):
     return np.random.uniform(action_spec.minimum, action_spec.maximum, size=action_spec.shape)
 
 viewer.launch(env, policy=random_policy)
+
+# print(env.physics.named.data.qpos)
