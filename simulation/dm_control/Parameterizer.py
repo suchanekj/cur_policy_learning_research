32abd9be45303a7af2886a0a39ae23700cdf2f8d
Ivin <rdrf.myfoot@gmail.com>
2020-12-12 14:20:22 +0000

    Added default values to parameterizer

--- simulation/dm_control/Parameterizer.py
+++ simulation/dm_control/Parameterizer.py
@@ -8,9 +8,25 @@ class Parameterizer:
     xml_folder = os.path.join(os.path.dirname(__file__), 'environments', 'assets')
     unmodified_lift = os.path.join(xml_folder, 'passive_hand_unmodified', 'lift.xml')
     unmodified_robot = os.path.join(xml_folder, 'passive_hand_unmodified', 'robot.xml')
-    
     modified_lift = os.path.join(xml_folder, 'passive_hand', 'lift.xml')
     modified_robot = os.path.join(xml_folder, 'passive_hand', 'robot.xml')
+
+    def randomize(self, i):
+        """
+        Randomizes the model parameters. Returns a tuple of all the randomized parameters.
+        Upper bound: ~20% difference from original values (modify if necessary!)
+
+        i -- input range: [0, 1], i.e. 0 to 1 --> 0% change to 20% change
+        """
+        if(i < 0 or i > 1): return None
+
+        # object_translate(self, dx, dy, dz)
+        # object_change_slope(self, r=0.025, rbtm=0.03, h=0.120, t=0.012)
+        # robot_change_joint_stiffness(self, v)
+        # robot_change_finger_spring_default(self, a)
+        # robot_change_thumb_spring_default(self, a, b, c)
+        # robot_change_friction(self, a, b, c)
+
 
     def __init__(self):
         """Initialises an xml tree for lift.xml and robot.xml"""
@@ -20,8 +36,6 @@ class Parameterizer:
 
         self.lift_root = self.lift_tree.getroot()
         self.robot_root = self.robot_tree.getroot()
-    # def num_to_str(self, *args):
-    #     return ' '.join(args)
 
     def _translate(self, pos: str, dx, dy, dz) -> str:
         """takes a position string, translates it by dxyz, and returns it"""
@@ -33,7 +47,7 @@ class Parameterizer:
         xyz = ' '.join(xyz)
         return xyz
 
-    def object_translate(self, dx, dy, dz):
+    def object_translate(self, dx=0, dy=0, dz=0):
         """modifies lift_root by shifting the object position by xyz"""
 
         # needs to be updated once dhilan adds in his objects
@@ -65,9 +79,11 @@ class Parameterizer:
             cylinder.attrib = dict(name='object0' + str(i), pos=nxtpos, size=nxtsz, type='cylinder', condim='3',
                                material='block_mat', mass='0')
             object.append(cylinder)
-            self.printer(cylinder)
 
-    def robot_change_joint_stiffness(self, v):
+    def robot_change_finger_length(self, v):
+        pass
+
+    def robot_change_joint_stiffness(self, v=80.0):
         """
         Changes the joint stiffness
         v -- stiffness value
@@ -96,18 +112,18 @@ class Parameterizer:
     #         elif not ("J3" in i.attrib['name'] or "J4" in i.attrib['name']):
     #             i.attrib['springref'] = str(b)
 
-    def robot_change_finger_spring_default(self, a):
+    def robot_change_finger_spring_default(self, a=0.2):
             """
             Changes default finger position by changing the default lengths for the springs in the proximal, middle and distal finger joints.
             a is in the range [0, 1.571], where 0 is for fully extended and 1.571 is for fully bent.
-            a -- default position
+            a -- default position for all three fingers (original: 0.2)
             """
             palm = [i for i in self.robot_root.iter('body') if i.get('name')=='robot0:palm'][0]
             for i in palm.iter('joint'):
                 if not any(_ in i.attrib['name'] for _ in ["J4","J3","TH"]):
                     i.attrib['springref'] = str(a)
                     
-    def robot_change_thumb_spring_default(self, a, b, c):
+    def robot_change_thumb_spring_default(self, a=0.1, b=1.6, c=-0.8):
             """
             Changes default thumb position by changing the default lengths for the thumb joints.
 
@@ -124,7 +140,7 @@ class Parameterizer:
                 elif(i.attrib['name'] == "robot0:THJ0"):
                     i.attrib['springref'] = str(c)
 
-    def robot_change_friction(self, a, b, c):
+    def robot_change_friction(self, a=1, b=0.005, c=0.0001):
         """
         Changes the friction in robot's fingertips
         a -- translational friction
@@ -153,8 +169,8 @@ class Parameterizer:
 
 pm = Parameterizer()
 # pm.robot_change_spring_default(1, 1)
-pm.robot_change_finger_spring_default(0)
-pm.robot_change_thumb_spring_default(1, 1.6, 0)
+# pm.robot_change_finger_spring_default(1.571)
+# pm.robot_change_thumb_spring_default(1, 1.6, 0)
 # pm.robot_change_joint_stiffness(5)
 # pm.object_change_slope(0.025, 0.04, 0.12, 0.001)
 # pm.translate_object(1, 1, 1)
--- simulation/dm_control/devel.py
+++ simulation/dm_control/devel.py
@@ -6,11 +6,7 @@ env = environments.load(domain_name='passive_hand', task_name='lift_sparse')
 action_spec = env.action_spec()
 
 def random_policy(time_step):
-    env.physics.named.data.xpos['object0'][2] = 1.6
-    # print(env.physics.named.data.xpos)
     return np.random.uniform(action_spec.minimum, action_spec.maximum, size=action_spec.shape)
-
-print(dir(env.physics.named.data))
 
 viewer.launch(env, policy=random_policy)
 
